085379288e042b6d7123208dd0be1d7434cf60b2
jela3105 <jela3105@gmail.com>
2021-05-21 23:49:22 -0500

    Show in client status online or offline

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,15 @@
+const lblOnline = document.getElementById("lblOnline");
+const lblOffline = document.getElementById("lblOffline");
+
 const socket = io();
+
+socket.on("connect", () => {
+  console.log("connected");
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+socket.on("disconnect", () => {
+  console.log("disconnect");
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+});
