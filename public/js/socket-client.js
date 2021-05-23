28cab9129b9317e94d82cbc69a57a7a882f037df
jela3105 <jela3105@gmail.com>
2021-05-22 23:26:25 -0500

    Send message from client

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,6 +1,7 @@
 const lblOnline = document.getElementById("lblOnline");
 const lblOffline = document.getElementById("lblOffline");
-
+const txtMessage = document.getElementById("txtMessage");
+const btnSend = document.getElementById("btn-send");
 const socket = io();
 
 socket.on("connect", () => {
@@ -12,4 +13,9 @@ socket.on("disconnect", () => {
   console.log("disconnect");
   lblOffline.style.display = "";
   lblOnline.style.display = "none";
+});
+
+btnSend.addEventListener("click", () => {
+  const message = txtMessage.value;
+  socket.emit("send-message", message);
 });
