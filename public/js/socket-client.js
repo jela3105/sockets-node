5bd9cba105f6b97a1cab1d3a28f611ef2de152d0
jela3105 <jela3105@gmail.com>
2021-05-23 22:18:19 -0500

    Add new frontend files for queue by turns

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,26 +1,47 @@
-const lblOnline = document.getElementById("lblOnline");
-const lblOffline = document.getElementById("lblOffline");
-const txtMessage = document.getElementById("txtMessage");
-const btnSend = document.getElementById("btn-send");
+
+// Referencias del HTML
+const lblOnline  = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar  = document.querySelector('#btnEnviar');
+
+
 const socket = io();
 
-socket.on("connect", () => {
-  lblOffline.style.display = "none";
-  lblOnline.style.display = "";
-});
-socket.on("disconnect", () => {
-  console.log("disconnect");
-  lblOffline.style.display = "";
-  lblOnline.style.display = "none";
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display  = '';
+
 });
 
-socket.on("send-message", (payload) => {
-  console.log(payload);
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    lblOnline.style.display  = 'none';
+    lblOffline.style.display = '';
 });
 
-btnSend.addEventListener("click", () => {
-  const message = txtMessage.value;
-  socket.emit("send-message", message, (id) => {
-    console.log("from the server ", id);
-  });
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log( payload )
+})
+
+
+btnEnviar.addEventListener( 'click', () => {
+
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+    
+    socket.emit( 'enviar-mensaje', payload, ( id ) => {
+        console.log('Desde el server', id );
+    });
+
 });
