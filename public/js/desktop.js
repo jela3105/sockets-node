88ff781c3f0808ad719a6eec44a87352101f560f
jela3105 <jela3105@gmail.com>
2021-05-25 20:55:54 -0500

    Prepare desktop screen

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,3 +1,24 @@
+const lblDesktop = document.querySelector("h1");
+const btnAttent = document.querySelector("button");
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has("Desktop")) {
+  window.location = "index.html";
+  throw new Error("The desktop is required");
+}
+
+const desktop = searchParams.get("Desktop");
+lblDesktop.innerText = desktop;
+
+const socket = io();
+
+socket.on("connect", () => {
+  btnAttent.disable = false;
+});
+
+socket.on("disconnect", () => {
+  btnAttent.disable = true;
+});
+
+btnAttent.addEventListener("click", () => {});
