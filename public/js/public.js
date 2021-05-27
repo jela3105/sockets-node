028d2c822a02024f92fe87832c63dd6b2e01f781
jela3105 <jela3105@gmail.com>
2021-05-26 22:39:12 -0500

    Show actual ticket in public screen

--- public/js/public.js
+++ public/js/public.js
@@ -1,3 +1,22 @@
+const lblTicket1 = document.querySelector("#lblTicket1");
+const lblTicket2 = document.querySelector("#lblTicket2");
+const lblTicket3 = document.querySelector("#lblTicket3");
+const lblTicket4 = document.querySelector("#lblTicket4");
+const lblDesktop1 = document.querySelector("#lblDesktop1");
+const lblDesktop2 = document.querySelector("#lblDesktop2");
+const lblDesktop3 = document.querySelector("#lblDesktop3");
+const lblDesktop4 = document.querySelector("#lblDesktop4");
+const socket = io();
 
+socket.on("actual-state", (payload) => {
+  const [ticket1, ticket2, ticket3, ticket4] = payload;
+  lblTicket1.innerText = "Ticket " + ticket1.number;
+  lblTicket2.innerText = "Ticket " + ticket2.number;
+  lblTicket3.innerText = "Ticket " + ticket3.number;
+  lblTicket4.innerText = "Ticket " + ticket4.number;
 
-console.log('Público HTML')
+  lblDesktop1.innerText = ticket1.desktop;
+  lblDesktop2.innerText = ticket2.desktop;
+  lblDesktop3.innerText = ticket3.desktop;
+  lblDesktop4.innerText = ticket4.desktop;
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,6 +3,7 @@ const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
   socket.emit("last-ticket", ticketControl.last);
+  socket.emit("actual-state", ticketControl.lastFour);
   socket.on("next-ticket", (payload, callback) => {
     const next = ticketControl.next();
     callback(next);
@@ -15,6 +16,7 @@ const socketController = (socket) => {
       });
     }
     const ticket = ticketControl.attendTicket(desktop);
+    socket.broadcast.emit("actual-state", ticketControl.lastFour);
     if (!ticket) {
       callback({ ok: false, msg: "There are not more tickets" });
     } else {
